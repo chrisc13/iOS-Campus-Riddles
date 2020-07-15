529515346351a15507db1781feadeec97e5db84c
Chris Carbajal <43799053+chrisc13@users.noreply.github.com>
2020-07-15 15:23:17 -0700

    Initial Commit

---
+++ Campus Riddles/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Campus Riddles
+//
+//  Created by Chris Carbajal on 7/15/20.
+//  Copyright © 2020 Chris Carbajal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
